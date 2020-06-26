796bb1937f84fafec5a8e1376e2165a791e21dfc
johncalab <john.robert.calabrese@gmail.com>
2020-06-26 00:37:55 -0500

    need to figure out why loss is basically zero, but f1 is terrible

--- bald/simple_model.py
+++ bald/simple_model.py
@@ -3,7 +3,13 @@ from torch import nn
 import torch.nn.functional as F
 
 class ConllModel(nn.Module):
-    def __init__(self,max_seq_len,emb_dim,num_labels):
+    def __init__(
+        self,
+        emb_dim,
+        num_labels,
+        out_channels=100,
+        cnns=3,
+    ):
         super().__init__()
         """
         input tensor: x
@@ -14,27 +20,29 @@ class ConllModel(nn.Module):
         output of model: y_pred
             of shape (batch_len,seq_len,num_labels)
         """
+        assert isinstance(cnns,int)
+        assert cnns > 0
+
         self.num_labels = num_labels
 
         # cnn receives tensor of shape
         # (batch_len,1,seq_len,emb_dim)
         # outputs tensor of shape
         # (batch_len,out_channels,seq_len,1)
-        out_channels = 100
         cnn_kwargs = {
             "in_channels":1,
             "out_channels":out_channels,
             "kernel_size":(3,emb_dim),
             "padding":(1,0),
-        }        
-        self.cnn = nn.Conv2d(**cnn_kwargs)
+        }
+        self.convs = nn.ModuleList([nn.Conv2d(**cnn_kwargs) for _ in range(cnns)])
 
         # fc receives tensor of shape
         # (batch_len,seq_len,out_channels)
         # outputs tensor of shape
         # (batch_len,seq_len,num_labels)
         last_dim = out_channels
-        self.fc = nn.Linear(last_dim,num_labels)
+        self.fc = nn.Linear(cnns*last_dim,num_labels)
         
     def forward(self,x,mle_prediction=False):
         """
@@ -50,12 +58,21 @@ class ConllModel(nn.Module):
 
         """
         batch_len,seq_len,_ = x.size()
-        
         x = x.unsqueeze(dim=1)
-        x = self.cnn(x)
-        x = F.relu(x)
-        x = x.squeeze(dim=3)
-        x = x.view(batch_len,seq_len,-1)
+
+        # apply convolutions
+        x_out = []
+        for layer in self.convs:
+            t = layer(x)
+            t = F.relu(t)
+            t = t.squeeze(dim=3)
+            t = t.view(batch_len,seq_len,-1)
+            x_out.append(t)
+
+        # x_out = [F.relu(layer(x)).squeeze(dim=3).view(batch_len,seq_len,-1) for layer in self.convs]
+
+        # concatenate result
+        x = torch.cat(x_out, dim=2)
         x = self.fc(x)
         
         if mle_prediction is True:
